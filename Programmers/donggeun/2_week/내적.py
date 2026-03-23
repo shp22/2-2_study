c48fff63db23e50baa08508148f6c4c7dd024769
donggeun333 <donggeun3237@gmail.com>
2026-03-23 16:58:17 +0900

    Update 내적.py

--- Programmers/donggeun/2_week/내적.py
+++ Programmers/donggeun/2_week/내적.py
@@ -3,3 +3,18 @@ import numpy as np
 def solution(a, b):
 
     return int(np.dot(a,b))
+
+
+
+
+# 리스트 컴프리헨션 사용
+def solution(a, b):
+
+    return sum([x*y for x, y in zip(a,b)])
+
+# for문 사용
+def solution(a, b):
+    c = []
+    for i in range(len(a)):
+        c.append(a[i]*b[i])
+    return sum(c)
