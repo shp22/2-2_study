8f93e4910fcddb20a84f8be9b92f787586c148b1
leedonggeun <donggeun3237@gmail.com>
2026-03-30 16:35:13 +0900

    3week

--- Programmers/donggeun/2_week/내적.py
+++ Programmers/donggeun/2_week/내적.py
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
 import numpy as np
 
 def solution(a, b):
 
     return int(np.dot(a,b))
-=======
-import numpy as np
-
-def solution(a, b):
-
-    return int(np.dot(a,b))
-
-
 
 
 # 리스트 컴프리헨션 사용
@@ -25,4 +16,3 @@ def solution(a, b):
     for i in range(len(a)):
         c.append(a[i]*b[i])
     return sum(c)
->>>>>>> 59d5338ae07849a0a34935d6dc5686fb109f9689
--- Programmers/donggeun/2_week/정수나선형배치.py
+++ Programmers/donggeun/2_week/정수나선형배치.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def solution(n):
     answer = [[0] * n for _ in range(n)]
     
@@ -32,12 +31,3 @@ def solution(n):
         col_start += 1
             
     return answer
-=======
-def solution(n):
-    answer = [[0] * n for _ in range(n)]
-    
-    num = 1
-    row_start, row_end = 0, n - 1
-    col_start, col_end = 0, n - 1
-    
->>>>>>> 59d5338ae07849a0a34935d6dc5686fb109f9689
